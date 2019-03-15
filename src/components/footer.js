07b657c2c2805c60a85aaf68b6d8dd2b6f865817
aliu <adam.liu@tw.synaptics.com>
2019-03-15 13:34:49 +0800

    Add link of Adam & Alvin's Fun Lab in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,11 +3,16 @@ import Card from 'react-bootstrap/Card';
 
 class Footer extends React.Component {
   render() {
+    const LinkStyle = {
+    color: 'white',
+    textDecoration: "none",
+  };
+    
     return (
         <div>
           <Card style={{ margin: 5, background: "#3A7934", color: "white", borderColor:"#5DC928" }}>
             <Card.Body>
-            <strong>Copyright @ Adam & Alvin's Fun Lab</strong>
+            <strong>Copyright @ <a style={LinkStyle} href="https://shincar.github.io/blogs/">Adam & Alvin's Fun Lab</a></strong>
             </Card.Body>
           </Card>
         </div>)
